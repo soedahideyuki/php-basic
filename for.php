7b586eb1413bc1c01e735de6e2a6cdd9811990dd
soedahideyuki <kiyudehi811@gmail.com>
2023-12-19 23:31:12 +0900

    for文

---
+++ for.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP基礎編</title>
+</head>
+<body>
+  <p>
+    <?php
+    //1~10までを順番に出力する
+    for($i = 1;$i <= 10; $i++){
+      echo $i . '<br>';
+    }
+    ?>
+  </p>
+  <p>
+    <?php
+    for($i = 1;$i <=10 ;$i++){
+    $num = mt_rand(1,20);
+    echo $i . '回目の結果は' . $num . 'です';
+    echo '<br>';
+
+    if($num == 20){
+      echo '20が出たので繰り返し処理を強制終了します。';
+      break;
+    }
+    }
+    ?>
+  </p>
+  <p>
+    <?php
+    for($i == 1;$i <= 10; $i++){
+      if($i % 2 === 1){ 
+        continue;
+      }
+
+      echo $i . '<br>';
+    }
+    ?>
+  </p>
+</body>
+</html>
