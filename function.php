b9f6049b360d7dcfcfbb33250ae6f8a9494666e6
soedahideyuki <kiyudehi811@gmail.com>
2023-12-23 16:34:19 +0900

    関数完了

--- function.php
+++ function.php
@@ -1,3 +1,7 @@
+<?php
+//型宣言と異なるデータ型の場合にエラーが発生する。
+declare(strict_types=1); 
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -21,6 +25,52 @@
 
     say_good_morning();
     say_good_evening();
+    ?>
+  </p>
+  <p>
+    <?php
+    function calculate_total($price){
+      $total = $price + 500;
+      echo "{$total}円<br>";
+    }
+
+    calculate_total(1200);
+    
+
+    function add_two_arguments($price , $shipping_fee){
+      $total = $price + $shipping_fee;
+      echo "{$total}円<br>";
+    }
+
+    add_two_arguments(1200,500);
+    ?>
+  </p>
+  <p>
+    <?php
+    function double($num){
+      return $num * 2;
+    }
+
+    echo double(30);
+    ?>
+  </p>
+  <p>
+    <?php
+    function type_hinting_argument(int $num){  //int整数型（小数点を切り捨てる）
+      return $num * 2;
+    }
+
+    // echo type_hinting_argument(1.618);
+    echo type_hinting_argument(1);
+    ?>
+  </p>
+  <p>
+    <?php
+    function type_hinting_return_value($num): int{
+      return $num * 2;
+    }
+
+    echo type_hinting_return_value(1);
     ?>
   </p>
 </body>
